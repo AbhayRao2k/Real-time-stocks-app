db13d1a1d58326697a3adeb3bfc347044688cf58
Abhay Rao <abhayrao2k@gmail.com>
2025-10-09 11:35:52 +0530

    implemented header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import NavItems from './NavItems'
 import { UserDropdown } from './UserDropdown'
 
 const Header = () => {
   return (
-    <div>
-      <NavItems />
-      <UserDropdown />
-    </div>
-
+    <header className='sticky top-0 header '>
+      <div className='container header-wrapper'>
+        <Link href={'/'} className='text-2xl font-bold'>
+          <Image src="/assets/icons/logo.svg" alt='app logo' width={140} height={32} className='h-8 w-auto cursor-pointer' />
+        </Link>
+        <nav className='hidden sm:block'>
+          <NavItems />
+        </nav>
+          <UserDropdown />
+      </div>
+    </header>
   )
 }
 
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,17 +1,40 @@
+"use client"
+
 import { NAV_ITEMS } from '@/lib/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const NavItems = () => {
+    const pathname = usePathname()
+
+    const isActive = (path: string) => {
+        if (path === '/') return pathname === '/'
+
+        return pathname.startsWith(path)
+    }
+
     return (
         // <ul className='flex gap-10 justify-center py-5'>
         //     <li>Dashboard</li>
         //     <li>Search</li>
         //     <li>Watchlist</li>
         // </ul>
-        NAV_ITEMS.map((href, title)=> (
-            <li key={href}>{href , title}</li>
-        ))
+
+        // We are rendering the list of navigation items using map function and they're coming from lib/constants.ts file
+        <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
+            {
+                NAV_ITEMS.map(({ href, label }) => (
+                    <li key={href}>
+                        <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href) ? 'text-gray-100' : ''}`}> {label} </Link>
+                    </li>
+                ))
+            }
+        </ul>
     )
+
 }
+
+
 
 export default NavItems
